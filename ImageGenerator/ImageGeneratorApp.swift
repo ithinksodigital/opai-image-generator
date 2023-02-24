51fb69cad44c8c89a81b1e8812e343a7d3163c8b
Rafal Pawelec (PL) <rafal.pawelec@pwc.com>
2023-02-24 12:03:23 +0100

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by Rafal Pawelec on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

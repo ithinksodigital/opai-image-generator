516ade8a1fb4aa0714065df580dfbbedc93b8e92
Rafal Pawelec <pawelec.rafal@gmail.com>
2023-07-07 14:07:49 +0200

    first commit

--- ImageGenerator/ContentView.swift
+++ ImageGenerator/ContentView.swift
@@ -6,16 +6,85 @@
 //
 
 import SwiftUI
+import OpenAIKit
+
+final class ViewModel: ObservableObject {
+    private var openai: OpenAI?
+    func setup() {
+        openai = OpenAI(Configuration(
+            organization: "Personal",
+            apiKey: ""
+        ))
+    }
+    
+    func generateImage(prompt: String) async -> UIImage? {
+        guard let openai = openai else {
+            return nil
+            
+            
+        }
+        
+        do {
+            let params = ImageParameters(
+                prompt: prompt,
+                resolution: .medium,
+                responseFormat: .base64Json
+            )
+            
+            let result = try await openai.createImage(
+                parameters: params
+            )
+            let data = result.data[0].image
+            let image = try openai.decodeBase64Image(data)
+            return image
+            
+        }
+        catch {
+            print(String(String(describing: error)))
+            return nil
+        }
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var image: UIImage?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 300)
+                }
+                else {
+                    Text("Type prompt to generate image")
+                }
+                Spacer()
+                TextField("Type prompt here...", text: $text)
+                    .padding()
+                Button("Generate") {
+                    if !text.trimmingCharacters(in: .whitespaces).isEmpty {
+                        Task {
+                            let result = await viewModel.generateImage(prompt: text)
+                            if result == nil {
+                                print("Failed to get image")
+                            }
+                            self.image = result
+                        }
+                    }
+              
+                }
+                
+            }
+            .navigationTitle("DALL-E Image Gen")
+            .onAppear {
+                viewModel.setup()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
